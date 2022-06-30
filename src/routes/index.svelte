13c04dd9eea89055da4ed22bb9faa96f1abec2f5
ajr250571 <ajr250571@gmail.com>
2022-06-30 17:45:38 -0300

    commit cc

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,23 +5,34 @@
 	let searchName = 'Buenos Aires';
 	let promise;
 
-	$: if (searchName.length > 4) {
+	$: if (searchName.length > 3) {
 		promise = getWeatherFrom(searchName);
 	} else {
 		promise = '';
 	}
 </script>
 
-<input
-	type="text"
-	bind:value={searchName}
-	placeholder="Buscar ..."
-	class="input input-bordered w-full max-w-xs mx-8"
-/>
+<div class="form-control w-full max-w-xs mx-8">
+	<!-- svelte-ignore a11y-label-has-associated-control -->
+	<label class="label">
+		<span class="label-text font-bold">Cual es su Ciudad ?</span>
+	</label>
+	<input
+		type="text"
+		bind:value={searchName}
+		placeholder="Buscar ..."
+		class="input input-bordered input-primary w-full max-w-xs"
+	/>
+</div>
 
 {#await promise}
-	<h2 class="text-secondary text-2xl mx-2">Procesando ...</h2>
+	<h2 class="text-secondary text-2xl mx-8 py-4">Procesando ...</h2>
 {:then weather}
+	#{#if weather.is_day==1}
+		<div data-theme="light"></div>
+	{:else}
+		<div data-theme="dark"></div>
+	{/if}
 	<div class="card w-96 bg-base-100 shadow-xl mx-2 my-2">
 		<figure><img src={weather.icon} alt={weather.name} /></figure>
 		<div class="card-body">
