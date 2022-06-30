3ee9cf2a86b4eefa5bafb64fa3d6feff3ebadb19
ajr250571 <ajr250571@gmail.com>
2022-06-29 21:56:34 -0300

    Commit Weather

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div data-theme="light" class="container py-4 px-2 max-h-full min-w-full">
+	<slot />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { getWeatherFrom } from '../services/weather.js';
+
+	const promise = getWeatherFrom('Tucuman');
+</script>
+
+{#await promise then weather}
+	<div class="card w-96 bg-base-100 shadow-xl mx-2 my-2">
+		<figure><img src={null} alt={weather.name} /></figure>
+		<div class="card-body">
+			<h2 class="card-title">{weather.name}</h2>
+			<p>{weather.region}</p>
+			<p>{weather.country}</p>
+			<p>{weather.temp_c}<sup>o</sup>C</p>
+			<h4 class='rotate-90 position: absolute'>{weather.text}</h4>
+			
+		</div>
+	</div>
+{:catch error}
+	<!-- promise was rejected -->
+{/await}
